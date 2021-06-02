2cd7f4bc681c08c83299ce9ec46edc73d22661fc
CaritoVillegas <ancasvillegas@hotmail.com>
2021-06-01 19:02:36 -0500

    Imagenes de personaje

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,8 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1500, 900, 1); 
+        addObject(new CaperuzaIn(), 500,500);
+        addObject(new CaperuzaG(), 600,600);
     }
 }
