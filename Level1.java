1f142e71af19661f88c04d3610a0cac88f16a970
CaritoVillegas <ancasvillegas@hotmail.com>
2021-06-12 13:01:00 -0500

    #15 Clase CaperuzaGame completa

--- Level1.java
+++ Level1.java
@@ -14,12 +14,18 @@ public class Level1 extends Levels
      * 
      */
     private static CaperuzaGame caperuza;
+    private int lives;
+    private static int points=0;
     public Level1()
     {
         super();
         map();
+        scoreBoard();
         caperuza=new CaperuzaGame();
         chacters();
+        points=0;
+        lives=caperuza.getlives();
+        showText(" = " + lives, 1430, 30);
     }
 
     public void map(){
@@ -31,16 +37,24 @@ public class Level1 extends Levels
             this.addObject(new Wall(), 0, y);
             this.addObject(new Wall(), 1500, y);
         }
-        for(int x=0; x < 1550; x+=72){
-            this.addObject(new Scoreboard(), x, 20);
-        }
-        showText("LEVEL 1", 750, 20);
-        addObject(new TeleportLevel1(),1460,830); 
+
     }
 
     public void chacters(){
         addObject(caperuza, 1400, 820);
         addObject(new Wolf(),750,450);
         addObject(new Spider(),750,450);
+        addObject(new Apple(),70,790);
+        addObject(new Berry(), 1030, 500);
+    }
+
+    public void scoreBoard(){
+        for(int x=0; x < 1550; x+=72){
+            this.addObject(new Scoreboard(), x, 20);
+        }
+        addObject(new Points(), 30, 20);
+        showText("LEVEL 1", 750, 20);
+        addObject(new TeleportLevel1(),1460,830); 
+        addObject(new Lives(), 1400,30);
     }
 }
